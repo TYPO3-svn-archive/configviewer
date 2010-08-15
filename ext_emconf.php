121fee4b5b867cb5ae3dbea7d3b940b4bfad3d34
t3peter <t3peter@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-15 21:34:24 +0000

    Initial commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/configviewer/trunk@36793 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "configviewer"
+#
+# Auto generated 10-11-2009 23:21
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'ConfigViewer',
+	'description' => 'JSON-Services to Explain this Typo3 Installation on a central Point. Allowed Hosts can see installed Extensions or used TypoScript for ease of Administration or simply as working reference',
+	'category' => 'services',
+	'author' => 'Peter Pietzschmann',
+	'author_email' => 'peter@pietzschmann.de',
+	'shy' => 1,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"d031";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_configviewer_eID.php";s:4:"10c5";s:21:"ext_conf_template.txt";s:4:"48c0";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"6c84";s:12:"lib/json.php";s:4:"6911";}',
+);
+
+?>
